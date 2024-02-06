6cedd8b8561ccb2a6adf947a5f781934db5c9c4c
Alasdair Orr <158096722+49ersMcAfree@users.noreply.github.com>
2024-02-06 16:20:56 -0600

    Update Student.java
    Updated function for student

--- Homework_2/src/student/edu/Student.java
+++ Homework_2/src/student/edu/Student.java
@@ -1,26 +1,62 @@
-/**
- * 
- */
 package student.edu;
 
-/**
- * 
- */
 public class Student {
+    
+    private int id;
+    private String name;
+    private double grade;
 
-	/**
-	 * 
-	 */
-	public Student() {
-		// TODO Auto-generated constructor stub
-	}
+    public Student() {
+        // Default constructor
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Parameterized constructor
+    public Student(int id, String name, double grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
 
-	}
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for grade
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    // toString method to display student information
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+
+    // Main method for demonstration
+    public static void main(String[] args) {
+        Student student1 = new Student(1, "John", 85.5);
+        System.out.println(student1.toString());
+    }
 }
